6e7c2e0698f20c41a5ea9be611b296f1cf4c4c17
ducks games <temetzianimmanuel@gmail.com>
2021-10-03 19:06:19 +0300

    lot of shit lol

---
+++ Assets/scripts/craftshitopenerl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class craftshitopenerl : MonoBehaviour
+{
+    public GameObject firstperson;
+    public GameObject Object;
+
+    public void objectshit()
+    {
+        Object.SetActive(!Object.activeSelf);
+    }
+    public void objectshit2()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            objectshit();
+
+            if (Object.activeInHierarchy == true)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            if (Object.activeInHierarchy == false)
+            {
+                objectshit2();
+            }
+        }
+
+    }
+}
+
